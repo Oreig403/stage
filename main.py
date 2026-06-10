d4319ee540f78490b8479af75c468c8ed8a283f1
Gwenole Urvoy de Portzamparc <gwenole2p@gmail.com>
2026-06-10 14:04:31 +0200

    Switched to mnist

--- main.py
+++ main.py
@@ -2,27 +2,14 @@ from matplotlib import pyplot as plt
 import tensorflow.keras as keras
 
 import numpy as np
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
-from keras.utils import to_categorical
-from keras import layers, models
+from keras import layers, models, datasets
+from keras.datasets import mnist
 
 def show_img(arr, vmin = 0, vmax = 255):
     plt.imshow(arr, interpolation='nearest', cmap='gray', vmin=vmin, vmax=vmax)
     plt.show()
 
-# 1. Load the dataset
-digits = load_digits()
-X = digits.images  # Shape: (1797, 8, 8)
-Y = digits.target  # Shape: (1797,)
-
-X = X / 16.0 
-
-# 3. One-hot encode the labels for Keras
-Y = to_categorical(Y, num_classes=10)
-
-# 4. Split into training and testing sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+(X_train, Y_train), (X_test, Y_test) = keras.datasets.mnist.load_data()
 
 print(f"X_train shape: {X_train.shape}")
 print(f"Y_train shape: {Y_train.shape}")
